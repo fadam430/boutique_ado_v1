5cf18b496eb4e9cd4047c2433933901778122e8e
fadam430 <f.adam430@gmail.com>
2026-02-21 17:49:47 +0000

    completed homepage header and CSS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -150,6 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # global static files directory
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # directory to store uploaded media files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
