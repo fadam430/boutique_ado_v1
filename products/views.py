cb057fa542c2eb664f73e68608a18182b7b485e0
fadam430 <f.adam430@gmail.com>
2026-02-22 13:42:15 +0000

    added category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
@@ -9,8 +9,14 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None # Initialize query variable to None to avoid potential UnboundLocalError
+    category = None # Initialize category variable to None to avoid potential UnboundLocalError
     
     if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category'].split(',') # Split the category string into a list of categories
+            products = products.filter(category__name__in=category) # Filter the products queryset based on the selected categories
+            category = Category.objects.filter(name__in=category) # Get the Category objects for the selected categories to display in the template
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -22,6 +28,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_string': query, # Pass the search query to the template context to pre-fill the search input field with the user's search term
+        'current_categories': category, # Pass the current categories to the template context to display the selected categories in the template
     }
 
     return render(request, 'products/products.html', context)
