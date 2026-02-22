8bdcae49f0f240b22832fc0df2ed5fc14c4d26bc
fadam430 <f.adam430@gmail.com>
2026-02-22 11:07:36 +0000

    Added product views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # allauth urls
     path('', include('home.urls')), # home app urls
+    path('products/', include('products.urls')), # products app urls
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
