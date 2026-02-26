f3e072b88acaaceeafc17ef60a8bb77d09a16d83
fadam430 <f.adam430@gmail.com>
2026-02-26 17:36:44 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,5 +15,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N8n9sHh7qj2mL1Xo5l3Zt0aQyKpVh6u7v8w9x0y1z2a3b4c5d6e7f8g9h0i1j2k3l4m5n6o7p8q9r0s1t2u3v4w5x6y7z8',
+        'client_secret': 'test client_secret_1234567890abcdefg',
     }
     return render(request, template, context)
