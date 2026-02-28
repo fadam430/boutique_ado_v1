f4f51a09c03952213ee2798715f04dd1318af1d5
fadam430 <f.adam430@gmail.com>
2026-02-28 09:21:15 +0000

    completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,12 +11,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OderLineItemAdminInline,)
     
     readonly_fields = ( 'order_number', 'date',
-                        'delivery_cost', 'order_total', 'grand_total')
+                        'delivery_cost', 'order_total', 'grand_total',
+                        'original_bag', 'stripe_pid',)
     
     fields = (  'order_number', 'full_name', 'email', 
                 'phone_number', 'country', 'postcode',
                 'town_or_city', 'street_address1', 'street_address2',
-                'county', 'date', 'delivery_cost', 'order_total', 'grand_total')
+                'county', 'date', 'delivery_cost', 'order_total', 'grand_total',
+                'original_bag', 'stripe_pid',)
 
     list_display = ('order_number', 'full_name', 'date',
                     'order_total', 'delivery_cost', 'grand_total')
