3727aad3982992b2b81e8b84a0ab2adf7b870c7f
fadam430 <f.adam430@gmail.com>
2026-03-16 11:17:23 +0000

    small change

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6#fte#s(t1sx@-zzolt%$*@k6kk61-w_^5%hbk-6kw^njo1wys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
